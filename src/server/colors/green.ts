d0a2080932f26b984413a67f663cfc892dba0800
kdiaz <kdiaz@disastertech.com>
2025-03-03 06:04:34 -0500

    add green

--- src/server/colors/green.ts
+++ src/server/colors/green.ts
@@ -1,20 +1,25 @@
 import { Request, Response, Router } from "express";
 
-type GreenGetResponse = Response;
+type GreenGetResponse = Response<{ color: string } | { colorError: string }>;
 type GreenGetRequest = Request;
 
 export const greenRouter = Router();
 
 export const greenGetter = (req: GreenGetRequest, res: GreenGetResponse) => {
-  res.send({ });
+  if (Math.random() > 0.5) {
+    return res.status(500).send({ colorError: "Server error" });
+  }
+  res.send({ 
+    color: "green" 
+  });
 };
 
-type GreenPostResponse = Response;
-type GreenPostRequest = Request;
-
-export const greenPoster = (req: GreenPostRequest, res: GreenPostResponse) => {
+export const orangePoster = (
+  req: GreenGetRequest,
+  res: GreenGetResponse
+) => {
   res.sendStatus(200);
 };
 
 greenRouter.get("/", greenGetter);
-greenRouter.post("/", greenPoster);
+// greenRouter.post("/", greenPoster);
